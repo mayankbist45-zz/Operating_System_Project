578351c00b6a19ffc969ba62729f4588622bae33
Mayank Bist <mayankbist45@gmail.com>
2020-03-26 19:44:00 +0530

    First Test

---
+++ cmake-build-debug/CMakeFiles/OSProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OSProject.dir/main.c.obj"
+  "OSProject.exe"
+  "OSProject.exe.manifest"
+  "OSProject.pdb"
+  "libOSProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OSProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
