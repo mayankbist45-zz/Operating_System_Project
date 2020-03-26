03a440474fa1a4accac0dd8b21ba53f7f267f3f8
Mayank Bist <mayankbist45@gmail.com>
2020-03-26 20:12:22 +0530

    Initial Commit :- Added Problem Statement

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
+/*
+ * Problem Statement :-
+ *      Sudesh Sharma is a linux expert who wants to have an online system where he can
+ *      handle student queries. Since there can be multiple requests at any time he wishes
+ *      to dedicate a fixed amount of time to every request so that everyone gets a fair
+ *      share of his time. He will log into the System from 10am to 12am only. He wants to
+ *      have separate requests queues for students and faculty. Implement a strategy for the
+ *      same. The summary at the end of the session should include the total time he spent on
+ *      handling queries and average query time.
+ *
+ * Author :-
+ *      Mayank Bist (11814526)
+ *
+ * Solution Implemented :-
+ *      Since we are planning to have different queues for students and faculties and each
+ *      of them needs a fixed time quantum and in order to maximize the efficiency i will be
+ *      calculating optimal time quantum to get the maximum efficiency in the shortest period
+ *      of time.
+ */
+
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+
     return 0;
 }
